09734374818172226177c73f7c475b9d6c54a31a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-31 18:17:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,8 @@
     artist_name, song_name, genre = filename.split(" - ")
     song = new(song_name)
     song.artist = artist_name(artist_name)
-    song.artist.add
+    song.artist.add_song(song)
+    song.artist.save
     song
   end
   
